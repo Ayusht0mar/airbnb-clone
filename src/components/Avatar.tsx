2effaece1f16510c2495c5bd441eda85052a91c3
Ayush tomar <ayushtomar388@gmail.com>
2024-12-01 20:44:55 +0530

    Created Avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import Image from "next/image";
+
+const Avatar = () => {
+    return ( 
+        <div>
+            <Image
+                src=""
+                className="rounded-full"
+                width={30}
+                height={30}
+                alt="Avatar"
+            />
+        </div>
+     );
+}
+ 
+export default Avatar;
