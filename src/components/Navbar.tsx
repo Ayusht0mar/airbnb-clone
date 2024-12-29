3e2cb49a90bd3e7a9efd6155be60ccef6e19d53f
Ayush tomar <ayushtomar388@gmail.com>
2024-12-29 15:49:06 +0530

    Category added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { useRouter } from "next/navigation";
 import Container from "./Container";
 import Image from "next/image"
 import { BiSearch } from "react-icons/bi";
@@ -7,6 +6,10 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "./Avatar";
 import { useCallback, useState } from "react";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import Categories from "./Categories";
+import { useRouter } from "next/navigation";
+import RentModal from "./modals/RentModal";
+import useRentModal from "@/hooks/useRentModal";
 
 const Navbar = () => {
     return ( 
@@ -20,6 +23,7 @@ const Navbar = () => {
                     </div>
                 </Container>
             </div>
+            <Categories/>
         </div>
      );
 };
@@ -27,8 +31,11 @@ const Navbar = () => {
 export default Navbar;
 
 const Logo = () => {
+    const router = useRouter();
+    
     return (
         <Image
+            onClick={() => router.push("/")}
             alt="Airbnb Logo"
             src="/images/logo.png"
             width={100}
@@ -55,6 +62,7 @@ const Search = () => {
 
 const UserMenu = () => {
     const registerModal = useRegisterModal();
+    const rentModal = useRentModal();
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = useCallback(() => {
@@ -64,7 +72,7 @@ const UserMenu = () => {
     return ( 
         <div className="relative">
             <div className="flex items-center gap-3">
-                <div onClick={() => {}} className="hidden md:block text-sm fotn-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+                <div onClick={rentModal.onOpen} className="hidden md:block text-sm fotn-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
                     Airbnb your home
                 </div>
                 <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 bottom-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
@@ -82,10 +90,14 @@ const UserMenu = () => {
                             onClick={registerModal.onOpen}
                             label="Login"
                          />
-                        <MenuItem 
+                        <MenuItem
                             onClick={registerModal.onOpen}
                             label="Sign Up"
                          />
+                         <MenuItem 
+                            onClick={() => {}}
+                            label=""
+                        />
                     </div>
                 </div>
             )}
