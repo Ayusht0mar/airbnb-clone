df33d8e017302b6cfb2f9686ec0577586f7ff191
Ayush tomar <ayushtomar388@gmail.com>
2024-12-30 23:44:05 +0530

    Auth

---
+++ src/app/actions/getCurrentUser.ts
@@ -0,0 +1,33 @@
+import { auth } from "@/auth"
+import { prisma } from "../libs/prismadb";
+
+export async function getCurrentUser() {
+  try {
+    // Get session from Auth.js
+    const session = await auth()
+
+    if (!session?.user?.email) {
+      return null; // No user is logged in
+    }
+
+    // Fetch user from Prisma database
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+        return null
+    }
+
+    return {
+        ...currentUser,
+        createdAt: currentUser.createdAt.toISOString(),
+        updatedAt: currentUser.updatedAt.toISOString(),
+        emailVerified: currentUser.emailVerified?.toISOString() || null,
+      }
+    } catch (error: any) {
+      return null
+    }
+}
