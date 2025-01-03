d1e4a4fc315dfff29018a1073d377c8664a65314
Ayush tomar <ayushtomar388@gmail.com>
2025-01-03 17:26:28 +0530

    Debbugged

--- src/app/api/listings/[listingId]/route.ts
+++ src/app/api/listings/[listingId]/route.ts
@@ -8,15 +8,13 @@ interface IListingsParams {
 
 export async function DELETE(
   request: Request,
-  { params }: { params: IListingsParams }
+  listingId: { params: Promise<IListingsParams> } 
 ) {
   const currentUser = await getCurrentUser()
 
   if (!currentUser) {
     return NextResponse.error()
   }
-
-  const { listingId } = params
 
   if (!listingId || typeof listingId !== "string") {
     throw new Error("Invalid ID")
